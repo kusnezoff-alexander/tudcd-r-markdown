1ff8772b9e076687a49c3af25f60e3353d583ad8
alku662e <alexander.kusnezoff@mailbox.tu-dresden.de>
2025-10-11 17:32:18 +0200

    Fix mermaid diagram generation
    See https://gitlab.com/inkscape/inbox/-/issues/3268#note_2169136079

--- utils/mermaid.lua
+++ utils/mermaid.lua
@@ -17,13 +17,13 @@ end
 function CodeBlock(el)
   if el.classes:includes("mermaid") then
     local input_file = tempname(".mmd")
-    local output_file = tempname(".svg")
+    local output_file = tempname(".pdf")
 
     local f = io.open(input_file, "w")
     f:write(el.text)
     f:close()
 
-    local cmd = string.format('%s -i "%s" -o "%s" --scale %d', mmdc_cmd, input_file, output_file, scale)
+    local cmd = string.format('%s -i "%s" --outputFormat=pdf --pdfFit -o "%s" --scale %d', mmdc_cmd, input_file, output_file, scale)
     io.stderr:write("Running: " .. cmd .. "\n")
 
     if not run_cmd(cmd) then
